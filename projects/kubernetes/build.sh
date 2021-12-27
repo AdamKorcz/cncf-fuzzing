5f2a2e1496937b5fa1bc876f52d2ea8248a3bf2d
AdamKorcz <adam@adalogics.com>
2021-12-27 16:51:25 +0000

    rb

--- projects/kubernetes/build.sh
+++ projects/kubernetes/build.sh
@@ -41,7 +41,7 @@ cp $SRC/cncf-fuzzing/projects/kubernetes/*.go \
 
 
 go mod tidy && go mod vendor
-go get github.com/google/gofuzz/bytesource
+go get github.com/google/gofuzz@v1.2.0
 # disable this fuzzer for now
 #compile_go_fuzzer k8s.io/kubernetes/pkg/kubelet/server FuzzRequest fuzz_request
 
